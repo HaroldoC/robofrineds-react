8713b7ae65db18deb33abbffe35c3fb34ec2e714
Haroldo Carvalho <carvalhoh@gmail.com>
2023-11-30 14:23:41 -0300

    up

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "Shanna@melissa.tv",
+  },
+  {
+    id: 3,
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "Nathan@Yesenia.net",
+  },
+  {
+    id: 4,
+    name: "Patricia Lebsack",
+    username: "Karianne",
+    email: "Julianne.OConner@kory.org",
+  },
+  {
+    id: 5,
+    name: "Chelsey Dietrich",
+    username: "Kamren",
+    email: "Lucio_Hettinger@annie.ca",
+  },
+  {
+    id: 6,
+    name: "Dennis Schulist",
+    username: "Leo",
+    email: "Karley_Dach@jasper.biz",
+  },
+  {
+    id: 7,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+  },
+  {
+    id: 8,
+    name: "Nicholas Cage",
+    username: "Nick",
+    email: "Nick_cage@rosamond.biz",
+  },
+  {
+    id: 9,
+    name: "Marcos Leonardo",
+    username: "Leo",
+    email: "Mleo@Checkitout.biz",
+  },
+  {
+    id: 10,
+    name: "Clementina Faria",
+    username: "Tina",
+    email: "Tinafaria@karina.biz",
+  },
+];
