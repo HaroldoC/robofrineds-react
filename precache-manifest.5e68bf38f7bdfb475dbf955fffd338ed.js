c144a4dbc8e4ccc1399cb2cf50327cd00bb3faf5
Haroldo Carvalho <carvalhoh@gmail.com>
2023-12-15 17:30:56 -0300

    Updates

---
+++ precache-manifest.5e68bf38f7bdfb475dbf955fffd338ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "927c527bc896fdde09041f8c21c3a87a",
+    "url": "/robofriends-react/index.html"
+  },
+  {
+    "revision": "5c9caab680f5c9ce05b4",
+    "url": "/robofriends-react/static/css/2.a94b31b9.chunk.css"
+  },
+  {
+    "revision": "2f0a49298e49c5eff1c2",
+    "url": "/robofriends-react/static/css/main.ac476065.chunk.css"
+  },
+  {
+    "revision": "5c9caab680f5c9ce05b4",
+    "url": "/robofriends-react/static/js/2.99ae3eca.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/robofriends-react/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "2f0a49298e49c5eff1c2",
+    "url": "/robofriends-react/static/js/main.15d46987.chunk.js"
+  },
+  {
+    "revision": "4d7fd39a57c29dc811af",
+    "url": "/robofriends-react/static/js/runtime~main.e5da5365.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-react/static/media/SEGA.12a3db8b.woff"
+  }
+]);
